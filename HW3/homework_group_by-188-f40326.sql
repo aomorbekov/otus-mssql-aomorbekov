96769001af147ff28a20b8e27299c583720a28cb
Askar O. <omorbekov.a@gmail.com>
2026-03-03 10:41:29 +0500

    asd
    asd

--- HW3/homework_group_by-188-f40326.sql
+++ HW3/homework_group_by-188-f40326.sql
@@ -17,7 +17,7 @@ https://github.com/Microsoft/sql-server-samples/releases/tag/wide-world-importer
 -- Задание - написать выборки для получения указанных ниже данных.
 -- ---------------------------------------------------------------------------
 
-USE WideWorldImporters
+use WideWorldImporters
 
 /*
 1. Посчитать среднюю цену товара, общую сумму продажи по месяцам.
@@ -30,16 +30,20 @@ USE WideWorldImporters
 Продажи смотреть в таблице Sales.Invoices и связанных таблицах.
 */
 
-select 
-    YEAR(si.InvoiceDate) as Sales_Year,
-    MONTH(si.InvoiceDate) as Sales_Month,
-    AVG(sil.UnitPrice) as AvgPriceMonth,
-    SUM(sil.Quantity * sil.UnitPrice) as SumSalesMonth
+select
+    YEAR(si.InvoiceDate) as SalesYear,
+    MONTH(si.InvoiceDate) as SalesMonth,
+    AVG(sil.UnitPrice) as AvgPricePerMonth,
+    SUM(sil.Quantity * sil.UnitPrice) as TotalSalesPerMonth
 from Sales.Invoices as si
-join Sales.InvoiceLines as sil
-on si.InvoiceID = sil.InvoiceID
-group by YEAR(si.InvoiceDate), MONTH(si.InvoiceDate)
-order by Sales_Year, Sales_Month
+inner join Sales.InvoiceLines as sil
+    on sil.InvoiceID = si.InvoiceID
+group by
+    YEAR(si.InvoiceDate),
+    MONTH(si.InvoiceDate)
+order by
+    SalesYear,
+    SalesMonth;
 
 /*
 2. Отобразить все месяцы, где общая сумма продаж превысила 4 600 000
@@ -51,16 +55,21 @@ order by Sales_Year, Sales_Month
 
 Продажи смотреть в таблице Sales.Invoices и связанных таблицах.
 */
-select 
-    YEAR(si.InvoiceDate) as Sales_Year,
-    MONTH(si.InvoiceDate) as Sales_Month,
-    SUM(sil.Quantity * sil.UnitPrice) as SumSalesMonth
+
+select
+    YEAR(si.InvoiceDate) as SalesYear,
+    MONTH(si.InvoiceDate) as SalesMonth,
+    SUM(sil.Quantity * sil.UnitPrice) as TotalSalesPerMonth
 from Sales.Invoices as si
-join Sales.InvoiceLines as sil
-on si.InvoiceID = sil.InvoiceID
-group by YEAR(si.InvoiceDate), MONTH(si.InvoiceDate)
-having SUM(sil.UnitPrice * sil.Quantity) > 4600000
-order by Sales_Year, Sales_Month
+inner join Sales.InvoiceLines as sil
+    on sil.InvoiceID = si.InvoiceID
+group by
+    YEAR(si.InvoiceDate),
+    MONTH(si.InvoiceDate)
+having SUM(sil.Quantity * sil.UnitPrice) > 4600000
+order by
+    SalesYear,
+    SalesMonth;
 
 /*
 3. Вывести сумму продаж, дату первой продажи
@@ -79,21 +88,27 @@ order by Sales_Year, Sales_Month
 Продажи смотреть в таблице Sales.Invoices и связанных таблицах.
 */
 
-select 
-    YEAR(si.InvoiceDate) AS Sales_Year,
-    MONTH(si.InvoiceDate) AS Sales_Month,
+select
+    YEAR(si.InvoiceDate) as SalesYear,
+    MONTH(si.InvoiceDate) as SalesMonth,
     wsi.StockItemName,
-    SUM(sil.Quantity * sil.UnitPrice) AS SumSales,
-    MIN(si.InvoiceDate) AS FirstSaleDate,
-    SUM(sil.Quantity) AS TotalQuantity
-from Sales.Invoices AS si
-join Sales.InvoiceLines AS sil
-    on si.InvoiceID = sil.InvoiceID
-join Warehouse.StockItems AS wsi
-    on sil.StockItemID = wsi.StockItemID
-group by YEAR(si.InvoiceDate), MONTH(si.InvoiceDate), wsi.StockItemName
+    SUM(sil.Quantity * sil.UnitPrice) as TotalSales,
+    MIN(si.InvoiceDate) as FirstSaleDate,
+    SUM(sil.Quantity) as TotalQuantity
+from Sales.Invoices as si
+inner join Sales.InvoiceLines as sil
+    on sil.InvoiceID = si.InvoiceID
+inner join Warehouse.StockItems as wsi
+    on wsi.StockItemID = sil.StockItemID
+group by
+    YEAR(si.InvoiceDate),
+    MONTH(si.InvoiceDate),
+    wsi.StockItemName
 having SUM(sil.Quantity) < 50
-order by Sales_Year, Sales_Month, wsi.StockItemName;
+order by
+    SalesYear,
+    SalesMonth,
+    wsi.StockItemName;
 
 -- ---------------------------------------------------------------------------
 -- Опционально
@@ -101,4 +116,78 @@ order by Sales_Year, Sales_Month, wsi.StockItemName;
 /*
 Написать запросы 2-3 так, чтобы если в каком-то месяце не было продаж,
 то этот месяц также отображался бы в результатах, но там были нули.
+
 */
+;with Months as (
+    select
+        DATEFROMPARTS(YEAR(MIN(InvoiceDate)), MONTH(MIN(InvoiceDate)), 1) as MinMonthStart,
+        DATEFROMPARTS(YEAR(MAX(InvoiceDate)), MONTH(MAX(InvoiceDate)), 1) as MaxMonthStart
+    from Sales.Invoices
+    union all
+    select DATEADD(MONTH, 1, MinMonthStart), MaxMonthStart
+    from Months
+    where MinMonthStart < MaxMonthStart
+),
+MonthlySales as (
+    select
+        DATEFROMPARTS(YEAR(si.InvoiceDate), MONTH(si.InvoiceDate), 1) as MonthStart,
+        SUM(sil.Quantity * sil.UnitPrice) as TotalSalesPerMonth
+    from Sales.Invoices as si
+    inner join Sales.InvoiceLines as sil
+        on sil.InvoiceID = si.InvoiceID
+    group by DATEFROMPARTS(YEAR(si.InvoiceDate), MONTH(si.InvoiceDate), 1)
+)
+select
+    YEAR(m.MinMonthStart) as SalesYear,
+    MONTH(m.MinMonthStart) as SalesMonth,
+    ISNULL(ms.TotalSalesPerMonth, 0) as TotalSalesPerMonth
+from Months as m
+left join MonthlySales as ms
+    on ms.MonthStart = m.MinMonthStart
+order by
+    SalesYear,
+    SalesMonth
+option (maxrecursion 0);
+
+;with Months as (
+    select
+        DATEFROMPARTS(YEAR(MIN(InvoiceDate)), MONTH(MIN(InvoiceDate)), 1) as MinMonthStart,
+        DATEFROMPARTS(YEAR(MAX(InvoiceDate)), MONTH(MAX(InvoiceDate)), 1) as MaxMonthStart
+    from Sales.Invoices
+    union all
+    select DATEADD(MONTH, 1, MinMonthStart), MaxMonthStart
+    from Months
+    where MinMonthStart < MaxMonthStart
+),
+ItemMonthlySales as (
+    select
+        DATEFROMPARTS(YEAR(si.InvoiceDate), MONTH(si.InvoiceDate), 1) as MonthStart,
+        wsi.StockItemName,
+        SUM(sil.Quantity * sil.UnitPrice) as TotalSales,
+        MIN(si.InvoiceDate) as FirstSaleDate,
+        SUM(sil.Quantity) as TotalQuantity
+    from Sales.Invoices as si
+    inner join Sales.InvoiceLines as sil
+        on sil.InvoiceID = si.InvoiceID
+    inner join Warehouse.StockItems as wsi
+        on wsi.StockItemID = sil.StockItemID
+    group by
+        DATEFROMPARTS(YEAR(si.InvoiceDate), MONTH(si.InvoiceDate), 1),
+        wsi.StockItemName
+    having SUM(sil.Quantity) < 50
+)
+select
+    YEAR(m.MinMonthStart) as SalesYear,
+    MONTH(m.MinMonthStart) as SalesMonth,
+    ims.StockItemName,
+    ISNULL(ims.TotalSales, 0) as TotalSales,
+    ims.FirstSaleDate,
+    ISNULL(ims.TotalQuantity, 0) as TotalQuantity
+from Months as m
+left join ItemMonthlySales as ims
+    on ims.MonthStart = m.MinMonthStart
+order by
+    SalesYear,
+    SalesMonth,
+    ims.StockItemName
+option (maxrecursion 0);
